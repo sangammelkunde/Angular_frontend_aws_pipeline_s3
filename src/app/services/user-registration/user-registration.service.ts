0a0cfe5a35eedcd06d550105c42af292dbd20d3a
UG50TS <sangameshwar.melkunde@ing.com>
2022-09-24 17:11:07 +0530

    removed local endpoint and added elb url

--- src/app/services/user-registration/user-registration.service.ts
+++ src/app/services/user-registration/user-registration.service.ts
@@ -3,10 +3,10 @@ import { Injectable } from '@angular/core';
 import { UserModel } from 'src/app/model/UserModel.model';
 import {map} from 'rxjs/operators';
 
-export const API_URL="http://tweetapp-loadbalancer-1655628136.us-east-1.elb.amazonaws.com/api/v1.0/tweets/register";
-export const Users_URL ="http://tweetapp-loadbalancer-1655628136.us-east-1.elb.amazonaws.com/api/v1.0/tweets/users/all";
-export const Search_URL = "http://tweetapp-loadbalancer-1655628136.us-east-1.elb.amazonaws.com/api/v1.0/tweets/user/search/";
-export const Forgot_URL="http://tweetapp-loadbalancer-1655628136.us-east-1.elb.amazonaws.com/api/v1.0/tweets";
+export const API_URL="http://end-tweet-elb-1790219837.us-east-1.elb.amazonaws.com/api/v1.0/tweets/register";
+export const Users_URL ="http://end-tweet-elb-1790219837.us-east-1.elb.amazonaws.com/api/v1.0/tweets/users/all";
+export const Search_URL = "http://end-tweet-elb-1790219837.us-east-1.elb.amazonaws.com/api/v1.0/tweets/user/search/";
+export const Forgot_URL="http://end-tweet-elb-1790219837.us-east-1.elb.amazonaws.com/api/v1.0/tweets";
 @Injectable({
   providedIn: 'root'
 })
